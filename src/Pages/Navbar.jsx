18c565ff960bcb69a6514f83f2f6ac422a20be79
rajeev <rajeev.kumar.phe21@itbhu.ac.in>
2024-02-12 18:25:13 +0530

    added responsiveness to navbar

--- src/Pages/Navbar.jsx
+++ src/Pages/Navbar.jsx
@@ -1,10 +1,15 @@
 /** @format */
 
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Navbar.module.css";
-import logo from "../assets/react.svg";
+import { IoMdMenu } from "react-icons/io";
+import { IoClose } from "react-icons/io5";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggleBtn = () => {
+    setIsOpen((prev) => !prev);
+  };
   return (
     <section className={styles.wrapper}>
       <div className={styles.nav}>
@@ -18,7 +23,18 @@ const Navbar = () => {
           <button className={styles.button}>
             <a href="/login">Register Now!</a>
           </button>
+          {!isOpen && <IoMdMenu onClick={toggleBtn} className={styles.icons} />}
+          {isOpen && <IoClose onClick={toggleBtn} className={styles.icons} />}
         </div>
+        {isOpen && (
+          <div className={styles.menu}>
+            <a href="/">Home</a>
+            <a href="#">Schedule</a>
+            <a href="/contact">Speakers</a>
+            <a href="/contact">Events</a>
+            <a href="/about">Team</a>
+          </div>
+        )}
       </div>
     </section>
   );
